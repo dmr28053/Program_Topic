20fca36756023cb3d35dfab5df4d4605cbe46d44
itp136-spring2023-dmr28053 <dmr28053@email.vccs.edu>
2024-05-02 20:52:29 -0400

    Add project files.

---
+++ Program_Topic/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Quartz;
+using Quartz.Impl;
+
+namespace Program_Topic
+{
+    internal class Program
+    {
+        private static async Task Main(string[] args)
+        {
+            StdSchedulerFactory factory = new StdSchedulerFactory();
+            IScheduler scheduler = await factory.GetScheduler();
+
+            await scheduler.Start();
+
+            IJobDetail job = JobBuilder.Create<SimpleClock>()
+                .WithIdentity("job1", "group1")
+                .Build();
+
+            ITrigger trigger = TriggerBuilder.Create()
+                .WithIdentity("trigger1", "group1")
+                .StartNow()
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(10)
+                    .RepeatForever())
+                .Build();
+
+            Console.WriteLine("This program outputs the time every 10 seconds. Press any key to exit.");
+            await scheduler.ScheduleJob(job, trigger);
+
+            Console.ReadKey();
+        }
+
+        public class SimpleClock : IJob
+        {
+            public async Task Execute(IJobExecutionContext context)
+            {
+                await Console.Out.WriteLineAsync($"The time is currently {DateTime.Now.ToString("h:mm:ss tt")}.");
+            }
+        }
+
+    }
+}
